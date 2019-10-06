5be77f3de2aad227f1bf142ff8b58413c2b147d7
yujingbo <1935952410@qq.com>
2019-10-06 12:07:59 +0800

    x

---
+++ 09-flutter/17-画几张页面/20-flutter中的状态管理provider/lib/pages/tabs/Cart.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../provider/Counter.dart';
+
+class CartPage extends StatefulWidget {
+  CartPage({Key key}) : super(key: key);
+
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  void initState() {
+    super.initState();
+    print("cart");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // print('1111111111111----');
+    var counterProvider = Provider.of<Counter>(context);
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          counterProvider.incCount();         
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text("${counterProvider.count}",style: TextStyle(
+          fontSize: 50
+        )),
+      ),
+    );
+  }
+}
---
+++ 09-flutter/17-画几张页面/20-flutter中的状态管理provider/lib/pages/tabs/User.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; 
+import '../../provider/Counter.dart';
+
+
+class UserPage extends StatefulWidget {
+  UserPage({Key key}) : super(key: key);
+
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  @override
+  Widget build(BuildContext context) {
+   
+    var counterProvider=Provider.of<Counter>(context);   
+
+    return Center(
+      child: Text("${counterProvider.count}",style: TextStyle(
+          fontSize: 50
+        )),
+    );
+  }
+}
